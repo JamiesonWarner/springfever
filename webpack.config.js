8359c2f0af9aedade5f9727d2285b48ad19f3000
Jamie Warner <jamiesonwarner@gmail.com>
2016-04-20 23:39:23 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
   },
+  devtool: 'source-map', // if we want a source map 
+
   module: {
     loaders: [
       { test: /\.ts$/, loader: 'ts-loader' }
